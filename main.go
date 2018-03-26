c8d2d76bed0ef75852576442d9f51296bbab9341
Louis <louis.baumann@vrsg.ch>
2018-03-26 15:25:06 +0200

    test

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -22,58 +23,42 @@ func main() {
 	if err != nil {
 		logging.WithID("PERF-OP-1").Fatal(err)
 	}
+	//testing purpose:
+	var data = [2][650]int{}
+
 	for {
 		now := int(time.Now().Unix())
-		getMonitoringData(config, config.OSDS_UP_Endpoint, now, 1)
-		getMonitoringData(config, config.AVG_OSD_APPLY_LATENCY, now, 1)
+		data[0] = getMonitoringData(config, config.OSDS_UP_Endpoint, now, 1)
+		data[1] = getMonitoringData(config, config.AVG_OSD_APPLY_LATENCY, now, 1)
 
 		time.Sleep(60 * time.Minute)
 	}
 
 }
 
-func getMonitoringData(config *configuration.Config, endpoint string, timeStampTo, hoursInPast int) {
+func getMonitoringData(config *configuration.Config, endpoint string, timeStampTo, hoursInPast int) [650]int {
 
-	startTime := -time.Duration(hoursInPast) * time.Hour
-	start := int(time.Now().Add(startTime).Unix())
-	url := config.MonitoringHost + endpoint + "&start=" + strconv.Itoa(start) + "&end=" + strconv.Itoa(timeStampTo) + "&step=" + config.SamplingStepSize
-	logging.WithIDFields("PERF-OP-2").Info(url)
-	var bearer = "Bearer " + config.BearerToken
-	req, err := http.NewRequest("GET", url, nil)
-	req.Header.Add("authorization", bearer)
-	client := http.Client{}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		logging.WithID("PERF-OP-3").Error(resp, err)
-		return
-	}
-	if resp.StatusCode != 200 {
-		logging.WithID("PERF-OP-h97843f7").Error(resp.Status, err)
-		return
-	}
-
-	defer resp.Body.Close()
-
-	body, _ := ioutil.ReadAll(resp.Body)
-	result := model.GrafanaResult{}
-	err = json.Unmarshal(body, &result)
+	result, err := getGrafanaResultset(config, endpoint, timeStampTo, hoursInPast)
 	if err != nil {
 		logging.WithError("PERF-OP-h9u349u43", err)
 		log.Println(err)
-		return
-	} else {
-		logging.WithID("PERF-OP-0h8943o483f4o8").Info(result.Status)
+		return [650]int{}
 	}
+	logging.WithID("PERF-OP-0h8943o483f4o8").Info(result.Status)
+
 	// Compute the 50th, 90th, and 99th percentile.
 	q := quantile.NewTargeted(0.50, 0.90, 0.99)
+	res := [650]int{}
 	for _, res := range result.Data.Result[0].Values {
 		// tm := time.Unix(int64(res[0].(float64)), 0)
 		// if err != nil {
 		// 	panic(err)
 		// }
 		value, _ := strconv.ParseFloat(res[1].(string), 64)
+		ts, _ := strconv.ParseFloat(res[0].(string), 64)
+		res = append(res, ts)
 		q.Insert(value)
+
 	}
 
 	fmt.Println("perc50:", q.Query(0.50))
@@ -81,4 +66,36 @@ func getMonitoringData(config *configuration.Config, endpoint string, timeStampT
 	fmt.Println("perc99:", q.Query(0.99))
 	fmt.Println("count:", q.Count())
 
+	return res
+
+}
+
+func getGrafanaResultset(config *configuration.Config, endpoint string, timeStampTo, hoursInPast int) (model.GrafanaResult, error) {
+	result := model.GrafanaResult{}
+	startTime := -time.Duration(hoursInPast) * time.Hour
+	start := int(time.Now().Add(startTime).Unix())
+	url := config.MonitoringHost + endpoint + "&start=" + strconv.Itoa(start) + "&end=" + strconv.Itoa(timeStampTo) + "&step=" + config.SamplingStepSize
+	logging.WithIDFields("PERF-OP-2").Info(url)
+	var bearer = "Bearer " + config.BearerToken
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		logging.WithID("PERF-OP-2.974").Error(err)
+		return result, err
+	}
+	req.Header.Add("authorization", bearer)
+	client := http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		logging.WithID("PERF-OP-3").Error(resp, err)
+		return result, err
+	}
+	if resp.StatusCode != 200 {
+		logging.WithID("PERF-OP-h97843f7").Error(resp.Status, err)
+		return result, errors.New("request failed with error: " + resp.Status)
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	err = json.Unmarshal(body, &result)
+	return result, err
 }
