f0fe970b561f0072d9b2d6c22971777ba61cedaf
Louis <louis.baumann@vrsg.ch>
2018-04-17 16:47:20 +0200

    hopp

--- web/web.go
+++ web/web.go
@@ -55,9 +55,18 @@ func RunSmall(w http.ResponseWriter, r *http.Request) {
 	if monitoring.VerifyClusterStatus() && params["size"] == "small" {
 		w.Write([]byte("small started"))
 		go runSmallFio()
-		//fio.FioGenPlot()
+	}
+	if monitoring.VerifyClusterStatus() && params["size"] == "medium" {
+		w.Write([]byte("medium started"))
+		go runSmallFio()
+	}
+	if monitoring.VerifyClusterStatus() && params["size"] == "large" {
+		w.Write([]byte("large started"))
+		go runSmallFio()
+	} else if !monitoring.VerifyClusterStatus() {
+		w.Write([]byte("cluster not ready to run fio"))
 	} else {
-		w.Write([]byte("cluster not ready to run small"))
+		w.Write([]byte("wrong command to run small!!" + params["size"]))
 	}
 
 }
